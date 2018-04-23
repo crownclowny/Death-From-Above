dd01260f59dd00a743f2377e57adaf8a44fb95ff
Diego Hernandez <35781653+crownclowny@users.noreply.github.com>
2018-04-23 09:41:18 -0400

    additional content
    corrections

---
+++ Final Level/Level1/New Unity Project/Assets/Scripts/Destroying.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroying : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+        if (other.tag == "Floor")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
