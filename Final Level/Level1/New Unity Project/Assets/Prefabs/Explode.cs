37f5fc266bf081417e38de8e32f7842509ac3a99
crownclowny <35781653+crownclowny@users.noreply.github.com>
2018-04-25 16:05:17 -0500

    version1.2
    Additions

---
+++ Final Level/Level1/New Unity Project/Assets/Prefabs/Explode.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explode : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Floor")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
